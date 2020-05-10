98fc7276dd5643790c29852970f964403b212171
Robin La Rondelle <robinlarondelle@hotmail.com>
2020-05-10 13:53:32 +0200

    Transporter & Catalog service init

---
+++ transporter-service/src/server.js
@@ -0,0 +1,34 @@
+//Get the correct environmnent variables
+if (process.env.NODE_ENV == "development"){
+  require('dotenv').config({ path: "../env/dev.env" }) 
+} else {
+  require('dotenv').config({ path: "./environment/prod.env" })
+}
+
+const express = require('express')
+const morgan = require("morgan") //HTTP request logger
+const bodyParser = require('body-parser') //Pase request body to JSON
+const cors = require("cors") // Access control
+
+const app = express()
+const port = process.env.PORT || "3000"
+app.use(bodyParser.json()) //Parse request body to JSON
+if (process.env.NODE_ENV == "development") app.use(morgan("dev")) //dont show all logs when in production mode
+app.use(cors('*'))
+
+app.use("/test", (req, res, next) => {
+  res.status(200).json({"message": "succes!"}).end()
+})
+
+//Catch all non existing endpoints
+app.use("*", function (req, res, next) {
+  next(new ErrorMessage("EndpointNotFoundError", "Endpoint not found", 404))
+})
+
+//Error middleware
+app.use((err, req, res, next) => {
+  res.status(err.status || 404).json(err).send();
+})
+
+//Setup server on designated port
+app.listen(port, () => console.log("Server is running on port: " + port + "\n"))
