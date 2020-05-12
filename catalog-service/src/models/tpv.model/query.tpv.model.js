08a1fcd6070ad1748083d94fc78602199843b377
Sjoerdoh <sjoerdschepers@hotmail.nl>
2020-05-12 17:53:40 +0200

    Added full functionality for TPV logic and fixed routing

--- catalog-service/src/models/tpv.model/query.tpv.model.js
+++
@@ -1,7 +0,0 @@
-module.exports = class QueryTpvModel {
-    //This constructor doesn't validate anything because it only reads data from the database 
-    constructor(id, name) {
-    this.id = id
-    this.name = name
-    }
-}
