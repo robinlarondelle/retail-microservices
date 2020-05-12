2e745061c626b70ed661dc2d7d7b0376bc5309e3
Tom Schoonbeek <34373221+TJFSchoo@users.noreply.github.com>
2020-05-12 22:07:56 +0200

    Transport service bugfix

--- transporter-service/src/transporter.controller.js
+++ transporter-service/src/transporter.controller.js
@@ -2,30 +2,26 @@ const Error = require('./models/error.model')
 const Transporter = require('./models/transporter.model')
 
 module.exports = {
+    // Create a new transporter
+    create(req, res, next) {
+        const name = req.body.name
+        if (name != null) {
+            Transporter.findOne({name}).then(result => {
+                if (!result) {
+                    Transporter.create({name}).then(result => {
+                        res.status(200).json(result).end()
+                    }).catch(err => res.status(400).json(new Error(400, "Error creating new Transporter: " + err)).end())
+                } else {
+                    res.status(400).json(new Error(400, "Transporter already exists.")).end()
+                }
+            }).catch(err => res.status(400).json(new Error(400, err)).end())
+        } else res.status(409).json(new Error(409, "Transporter name not found in body."))
+    },
     // Gets all transporters
     getAll(req, res, next) {
         Transporter.find({})
             .then(result => res.status(200).json(result).end())
     },
-
-    // Create a new transporter
-    create(req, res, next) {
-        const name = req.body.name
-
-        if (name != null) {
-            Transporter.findOne({name}).then(person => {
-                if (person) {
-                    res.status(400).json(new Error(400, "Transporter already exists.")).end()
-                } else {
-                    Transporter.create({name}).then(result => {
-                        res.status(200).json(result).end()
-                    })
-                        .catch(err => res.status(400).json(new Error(400, "Error creating new Transporter: " + err)).end())
-                }
-            }).catch(err => res.status(400).json(new Error(400, err)).end())
-        } else res.status(409).json(new Error(409, "Transporter name not found in body."))
-    },
-
     // Gets one specific transporter with the given ID
     getById(req, res, next) {
         Transporter.find({id: req.params.id}).then(result => {
@@ -37,11 +33,9 @@ module.exports = {
             }
         })
     },
-
     // Removes the transporter from the database
     delete(req, res, next) {
         const id = req.params.id
-
         if (id) {
             Transporter.delete({id})
                 .then(result => res.status(200).json(result).end())
