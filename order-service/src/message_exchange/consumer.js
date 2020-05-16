10d84dc9ef5be9ed4696342db62cfc557c294b20
Tom Smits <tmsmits@gmail.com>
2020-05-16 17:22:25 +0200

    RabbitMQ

--- order-service/src/message_exchange/consumer.js
+++ order-service/src/message_exchange/consumer.js
@@ -1,18 +1,26 @@
-const Order = require('../models/order.schema');
-const publisher = require('../message_exchange/publisher');
-
+const mongoose = require("mongoose")
+const Order = require("../models/order.schema");
+const Product = require("../models/product.schema");
+const Transporter = require("../models/transporter.schema");
 module.exports = {
-    consumeMsg(message) {
-        let content = JSON.parse(message.content.toString())
+    consumeMsg(message, acknowledge) {
+        console.log(JSON.stringify(message.content.toString()))
         let key = message.fields.routingKey
-
-        console.log("RabbitMQ message received with key: " + key)
-        console.log("Message contains: " + message.content.toString())
-        if(key === "transporter.assigned"){
-            console.log("Detected Transporter assigned to order: ")
-
-            // Logica om Transporter toe te voegen aan Order
-
+        let content = JSON.parse(message.content.toString())
+        switch(key) {
+            case 'catalog.product.created':
+                const product = new Product({
+                    _id: content._id,
+                    name: content.name
+                })
+                console.log("hoer")
+                product.save()
+                acknowledge(message)
+                break;
+            case 'catalog.product.deleted':
+        
+        
+                break;
         }
-    }
+    },
 }
