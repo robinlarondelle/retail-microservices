b662b56910eb0819db1a6cfee9ed9fe08d718f38
Sjoerdoh <sjoerdschepers@hotmail.nl>
2020-05-16 14:16:35 +0200

    Updated SQL support

--- catalog-query-service/src/message_exchange/consumer.js
+++ catalog-query-service/src/message_exchange/consumer.js
@@ -1,4 +1,4 @@
-const conn = require('../databaseHandler');
+const conn = require('../databasehandler');
 
 module.exports = {
     consumeMsg(message, acknowledge) {
@@ -6,18 +6,29 @@ module.exports = {
         let content = JSON.parse(message.content.toString())
         switch(key) {
             case 'catalog.product.created':
+                conn.query('INSERT INTO product (`_id`, `NAME`, `description`, `price`, `tpv`) VALUES(?, ?, ?, ?, (SELECT `name` FROM `tpv` WHERE `_id` = ?));', 
+                [content._id, content.name, content.description, content.price, content.tpv], function(error, results, fields) {
+                    if (error) console.log("Something went wrong while trying to insert product into the DB: " + error)
+                    else acknowledge(message)
+                })
                 break;
             case 'catalog.product.deleted':
-                //TODO set product to inactive
+                conn.query('UPDATE `product` SET active = false WHERE _id = ?;',
+                [content._id], function(error, results, fields) {
+                    if (error) console.log("Something went wrong while updating a product in the DB: " + error)
+                    else acknowledge(message)
+                })
                 break;
             case 'catalog.tpv.created':
-                conn.query('INSERT INTO tpv (_id, NAME) VALUES(?, ?);', [content._id, content.name], function(error, results, fields) {
+                conn.query('INSERT INTO tpv (_id, NAME) VALUES(?, ?);', 
+                [content._id, content.name], function(error, results, fields) {
                     if (error) console.log("Something went wrong while trying to insert tpv into the DB: " + error)
                     else acknowledge(message)
                 })
                 break;
             case 'catalog.tpv.deleted':
-                conn.query('DELETE FROM tpv WHERE _id = (?);', [content._id], function(error, results, fields) {
+                conn.query('DELETE FROM tpv WHERE _id = (?);', 
+                [content._id], function(error, results, fields) {
                     if (error) console.log("Something went wrong while trying to remove tpv from the DB: " + error)
                     else acknowledge(message)
                 })
