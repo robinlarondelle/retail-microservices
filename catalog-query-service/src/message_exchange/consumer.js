6cb44f5631ce57cb53acb454de518cb34813c73a
Sjoerdoh <sjoerdschepers@hotmail.nl>
2020-05-16 12:54:41 +0200

    Update consumer.js

--- catalog-query-service/src/message_exchange/consumer.js
+++ catalog-query-service/src/message_exchange/consumer.js
@@ -11,15 +11,16 @@ module.exports = {
                 //TODO set product to inactive
                 break;
             case 'catalog.tpv.created':
-                // Insert third party vendor into the MySQL database
                 conn.query('INSERT INTO tpv (_id, NAME) VALUES(?, ?);', [content._id, content.name], function(error, results, fields) {
                     if (error) console.log("Something went wrong while trying to insert tpv into the DB: " + error)
-                    // Only if it has been saved correctly do we acknowledge the message and remove it from the queue
                     else acknowledge(message)
                 })
                 break;
             case 'catalog.tpv.deleted':
-                //TODO remove third party vendor from the database
+                conn.query('DELETE FROM tpv WHERE _id = (?);', [content._id], function(error, results, fields) {
+                    if (error) console.log("Something went wrong while trying to remove tpv from the DB: " + error)
+                    else acknowledge(message)
+                })
                 break;
         }
     },
