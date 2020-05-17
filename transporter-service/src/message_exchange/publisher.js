131d2d83bcd928b5dba78bafc8f532d4ba80f326
Sjoerdoh <sjoerdschepers@hotmail.nl>
2020-05-17 19:58:05 +0200

    Update publisher.js

--- transporter-service/src/message_exchange/publisher.js
+++ transporter-service/src/message_exchange/publisher.js
@@ -1,35 +1,38 @@
 const amqp = require('amqplib/callback_api');
+let messageQueue = Array()
 
 module.exports = {
     publishMsg(key, message) {
-
         // Connects to the RabbitMQ server
         amqp.connect('amqp://rabbitmq:5672', function(err, conn) {
             if (err){
                 console.log("Could not connect to RabbitMQ server")
-            }
+                messageQueue.insert({key: key, message: message})  
+            } else {
+                // Creates a channel to communicatie through
+                conn.createChannel(function(err, channel) {
+                    if (err){
+                        console.log("Could not create RabbitMQ channel")
+                        messageQueue.insert({key: key, message: message})  
+                    } else {
+                        let exchange = 'default'
 
-            // Creates a channel to communicatie through
-            conn.createChannel(function(err, channel) {
-                if (err){
-                    console.log("Could not create RabbitMQ channel")
-                }
-                let exchange = 'default'
+                        // Checks if the exchange 'default' exists, otherwise creates a new exchange of type 'topic'
+                        channel.assertExchange(exchange, 'topic', {
+                            // The exchange will survive a broker restart
+                            durable: true
+                        })
 
-                // Checks if the exchange 'default' exists, otherwise creates a new exchange of type 'topic'
-                channel.assertExchange(exchange, 'topic', {
-                    // The exchange will survive a broker restart
-                    durable: true
+                        // Publishes the message to the exchange
+                        channel.publish(exchange, key, Buffer.from(JSON.stringify(message)))
+                    }
                 })
 
-                // Publishes the message to the exchange
-                channel.publish(exchange, key, Buffer.from(JSON.stringify(message)))
-            })
-
-            // Closes the connection to the RabbitMQ server
-            setTimeout(function() {
-                conn.close();
-            }, 500);
+                // Closes the connection to the RabbitMQ server
+                setTimeout(function() {
+                    conn.close();
+                }, 500);
+            }
         });
     }
 }
